0a3bfece6e5d0424a210ebedfd3fee6070b3b66f
raj9745 <rajkumartiruwa280@gmail.com>
2025-06-03 13:43:54 +0545

    part1 exercise

--- courseinfo/src/components/Total.jsx
+++
@@ -1,9 +0,0 @@
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <p><strong>Number of exercises: {total}</strong></p>
-  )
-}
-
-export default Total
