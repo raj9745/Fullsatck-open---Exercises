d57efc07796207193c2b4cd798981410cd89d921
raj9745 <rajkumartiruwa280@gmail.com>
2025-06-23 19:19:19 +0545

    part2-d 12.13

--- part2/phoneBook/src/services/person.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons';
-
-const getAll = () => axios.get(baseUrl).then(response => response.data);
-
-const create = (newPerson) => axios.post(baseUrl, newPerson).then(res => res.data);
-const remove = (id) => axios.delete(`${baseUrl}/${id}`);
-export default { getAll, create,remove};
