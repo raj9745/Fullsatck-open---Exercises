02b289668b36d5e81f891688b7b8b199f5b810f2
raj9745 <rajkumartiruwa280@gmail.com>
2025-07-02 09:26:55 +0545

    3.2

---
+++ part3/phonebook backend/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+  let persons =[
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+];
+
+
+//   /api/persons route
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+});
+
+//  /info route
+app.get ('/info',(request,response)=>{
+   const count = persons.length
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${count} people</p><p>${date}</p>`)
+})
+// Start server
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
