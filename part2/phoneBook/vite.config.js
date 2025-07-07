acba3e4ae6c53de6afaed28cceb293a592f2b398
raj9745 <rajkumartiruwa280@gmail.com>
2025-07-07 13:29:11 +0545

    part3-b

--- part2/phoneBook/vite.config.js
+++ part2/phoneBook/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
-  // other config
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
