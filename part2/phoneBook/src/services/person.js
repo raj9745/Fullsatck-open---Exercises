e4cfb24398e86daeec7fad64dd826cf16adcffdc
raj9745 <rajkumartiruwa280@gmail.com>
2025-07-08 16:41:26 +0545

    part3-b merge

--- part2/phoneBook/src/services/person.js
+++ part2/phoneBook/src/services/person.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
-const baseUrl = '/api/persons';
+const baseUrl = 'api/persons/';
 
 const getAll = () => axios.get(baseUrl).then(response => response.data);
 
 const create = (newPerson) => axios.post(baseUrl, newPerson).then(res => res.data);
 const remove = (id) => axios.delete(`${baseUrl}/${id}`);
 const update = (id, newPerson) => axios.put(`${baseUrl}/${id}`, newPerson).then(res => res.data);
-export default { getAll, create, remove,update };
+
+
+export default { getAll, create, remove, update };
