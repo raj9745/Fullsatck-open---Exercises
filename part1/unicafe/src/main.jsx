e16e1e023908e1454ffd4c194097ce66af849d93
raj9745 <194067960+raj9745@users.noreply.github.com>
2025-08-28 18:31:27 +0545

    updated part1

--- part1/unicafe/src/main.jsx
+++ part1/unicafe/src/main.jsx
@@ -1,19 +1,13 @@
-<<<<<<<< HEAD:part1/courseinfo/src/main.jsx
-========
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
->>>>>>>> part1-c:part1/unicafe/src/main.jsx
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<<<<<<<< HEAD:part1/courseinfo/src/main.jsx
     <App />
 )
-========
+
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
->>>>>>>> part1-c:part1/unicafe/src/main.jsx
+  </React.StrictMode>
